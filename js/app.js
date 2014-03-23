e90708c9083d348d80edf7f9b58d24df3976e5a9
Olof Johansson <olof.johansson@oddhill.se>
2014-03-23 10:46:32 +0100

    Initial Firebase connection.
    Hells yeah this is still buggy...

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var walltrApp = angular.module('walltrApp', ['ngRoute']);
+var walltrApp = angular.module('walltrApp', ['ngRoute', 'firebase']);
 
 walltrApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
@@ -13,63 +13,84 @@ walltrApp.config(['$routeProvider', function($routeProvider) {
     otherwise({redirectTo: '/login'});
 }]);
 
-walltrApp.controller('WalltrCtrl', ['$location', '$scope', function($location, $scope) {
-  $scope.users = {
-    olof: {
-      name: 'Olof',
-      email: 'olof.johansson@oddhill.se'
-    },
-    linus: {
-      name: 'Linus',
-      email: 'linus@linusbohman.se'
-    },
-    bjorn: {
-      name: 'Björn',
-      email: 'bjorn.albertsson@oddhill.se'
-    },
-    adam: {
-      name: 'Adam',
-      email: 'adam.gerthel@oddhill.se'
-    }
-  };
-
-  if (!window.currentUser) {
-    $location.path('/login');
-  } else {
-    $scope.currentUserName = $scope.users[window.currentUser].name;
-  }
-
+walltrApp.controller('WalltrCtrl', ['$location', '$scope', '$firebase', function($location, $scope, $firebase) {
+  var walltrRef = new Firebase('https://walltr.firebaseio.com');
+  var posts = $firebase(new Firebase('https://walltr.firebaseio.com/posts'));
+  $scope.user = false;
   $scope.posts = [];
+
+  var auth = new FirebaseSimpleLogin(walltrRef, function(error, user) {
+    if (error) {
+      alert('Such wrong');
+    }
+    else if (user) {
+      $scope.user = user;
+      $location.path('/wall');
+    }
+    else {
+      $scope.user = false;
+      $location.path('/login');
+    }
+  });
+
+  if ($scope.user) {
+    $location.path('/wall');
+  }
 
   $scope.addPost = function(text) {
     var date = new Date();
 
-    $scope.posts.push({
-      user: window.currentUser,
+    posts.$add({
+      user: $scope.user.email,
       timestamp: date.getTime(),
       text: text,
-      comments: []
+      parent: false
     });
   };
 
   $scope.addComment = function(post, text) {
     var date = new Date();
 
-    post.comments.push({
-      user: window.currentUser,
+    posts.$add({
+      user: $scope.user.email,
       timestamp: date.getTime(),
-      text: text
+      text: text,
+      parent: post.id
     });
   };
 
-  $scope.login = function(name) {
-    if (!$scope.users.hasOwnProperty(name)) {
-      alert('Such wrong');
-      return;
-    }
-
-    window.currentUser = name;
-    $location.path('/wall');
+  $scope.login = function(email, password) {
+    auth.login('password', {
+      email: email,
+      password: password
+    });
   };
+
+  posts.$on('child_added', function(snapshot) {
+    var post = snapshot.snapshot.value;
+
+    if (!post.parent) {
+      $scope.posts.push({
+        id: snapshot.snapshot.name,
+        user: post.user,
+        timestamp: post.timestamp,
+        text: post.text,
+        children: []
+      });
+    }
+    else {
+      angular.forEach($scope.posts, function(value, key) {
+        if (value.id == post.parent) {
+          value.children.push({
+            id: snapshot.snapshot.name,
+            user: post.user,
+            timestamp: post.timestamp,
+            text: post.text,
+            children: []
+          });
+        }
+      });
+    }
+  });
 
 }]);
