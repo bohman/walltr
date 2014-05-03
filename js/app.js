0df573e4869ae9804303005e680546942a8e40eb
Olof Johansson <olof.johansson@oddhill.se>
2014-05-03 12:39:11 +0200

    Fixed initial loading when redirected to /wall.

--- js/app.js
+++ js/app.js
@@ -8,43 +8,17 @@ walltrApp.config(['$routeProvider', function($routeProvider) {
     }).
     when('/login', {
       templateUrl: '/templates/login.html',
-      controller: 'WalltrCtrl'
+      controller: 'LoginCtrl'
     }).
     otherwise({redirectTo: '/login'});
 }]);
 
-walltrApp.controller('WalltrCtrl', ['$location', '$scope', '$firebase', '$firebaseSimpleLogin', function($location, $scope, $firebase, $firebaseSimpleLogin) {
+walltrApp.controller('LoginCtrl', ['$location', '$scope', '$firebase', '$firebaseSimpleLogin', '$rootScope', function($location, $scope, $firebase, $firebaseSimpleLogin, $rootScope) {
   $scope.auth = $firebaseSimpleLogin(new Firebase('https://walltr.firebaseio.com'));
-  var posts = $firebase(new Firebase('https://walltr.firebaseio.com/posts'));
-  $scope.users = {
-    'olof.johansson@me.com': 'Olof Johansson',
-    'bjorn.albertsson@gmail.com': 'Björn Albertsson',
-    'linus@linusbohman.se': 'Linus Bohman',
-    'insats@gmail.com': 'Adam Gerthel'
-  };
-  $scope.posts = [];
 
-  $scope.addPost = function(text) {
-    var date = new Date();
-
-    posts.$add({
-      user: $scope.auth.user.email,
-      timestamp: date.getTime(),
-      text: text,
-      parent: false
-    });
-  };
-
-  $scope.addComment = function(post, text) {
-    var date = new Date();
-
-    posts.$add({
-      user: $scope.auth.user.email,
-      timestamp: date.getTime(),
-      text: text,
-      parent: post.id
-    });
-  };
+  $rootScope.$on('$firebaseSimpleLogin:login', function(error, user) {
+    $location.path('/wall');
+  });
 
   $scope.login = function(email, password) {
     $scope.auth.$login('password', {
@@ -57,54 +31,128 @@ walltrApp.controller('WalltrCtrl', ['$location', '$scope', '$firebase', '$fireba
     });
   };
 
-  posts.$on('child_added', function(snapshot) {
-    var post = snapshot.snapshot.value;
+}]);
 
-    if (!post.parent) {
-      $scope.posts.push({
-        id: snapshot.snapshot.name,
-        user: post.user,
-        timestamp: post.timestamp,
-        text: post.text,
-        children: []
-      });
-    }
-    else {
-      angular.forEach($scope.posts, function(value, key) {
-        if (value.id == post.parent) {
-          value.children.push({
-            id: snapshot.snapshot.name,
-            user: post.user,
-            timestamp: post.timestamp,
-            text: post.text,
-            children: []
-          });
-        }
-      });
-    }
+walltrApp.controller('WalltrCtrl', ['$location', '$scope', '$firebase', '$firebaseSimpleLogin', '$rootScope', function($location, $scope, $firebase, $firebaseSimpleLogin, $rootScope) {
+  $scope.auth = $firebaseSimpleLogin(new Firebase('https://walltr.firebaseio.com'));
+
+  $rootScope.$on('$firebaseSimpleLogin:logout', function(error, user) {
+    $location.path('/login');
   });
 
-  posts.$on('child_removed', function(snapshot) {
-    var removedPost = snapshot.snapshot.value;
+  $rootScope.$on('$firebaseSimpleLogin:login', function(error, user) {
+    var posts = $firebase(new Firebase('https://walltr.firebaseio.com/posts'));
+    $scope.users = {
+      'olof.johansson@me.com': 'Olof Johansson',
+      'bjorn.albertsson@gmail.com': 'Björn Albertsson',
+      'linus@linusbohman.se': 'Linus Bohman',
+      'insats@gmail.com': 'Adam Gerthel'
+    };
+    $scope.posts = [];
 
-    if (!removedPost.parent) {
-      angular.forEach($scope.posts, function(post, key) {
-        if (post.id == snapshot.snapshot.name) {
-          $scope.posts.splice(key, 1);
+    $scope.addPost = function(text) {
+      var date = new Date();
+
+      posts.$add({
+        user: $scope.auth.user.email,
+        timestamp: date.getTime(),
+        text: text,
+        parent: false
+      });
+    };
+
+    $scope.addComment = function(post, text) {
+      var date = new Date();
+
+      posts.$add({
+        user: $scope.auth.user.email,
+        timestamp: date.getTime(),
+        text: text,
+        parent: post.id
+      });
+    };
+
+    posts.$on('loaded', function() {
+      angular.forEach(posts, function(value, key) {
+        if (typeof value == 'object') {
+          var post = value;
+          var id = key;
+
+          if (!post.parent) {
+            $scope.posts.push({
+              id: id,
+              user: post.user,
+              timestamp: post.timestamp,
+              text: post.text,
+              children: []
+            });
+          }
+          else {
+            angular.forEach($scope.posts, function(value, key) {
+              if (value.id == post.parent) {
+                value.children.push({
+                  id: id,
+                  user: post.user,
+                  timestamp: post.timestamp,
+                  text: post.text ? post.text : '',
+                  children: []
+                });
+              }
+            });
+          }
         }
       });
-    }
-    else {
-      angular.forEach($scope.posts, function(post, key) {
-        if (post.id == removedPost.parent) {
-          angular.forEach(post.children, function(child, key) {
-            if (child.id == snapshot.snapshot.name) {
-              post.children.splice(key, 1);
-            }
-          });
-        }
-      });
-    }
+    });
+
+    posts.$on('child_added', function(snapshot) {
+      var post = snapshot.snapshot.value;
+
+      if (!post.parent) {
+        $scope.posts.push({
+          id: snapshot.snapshot.name,
+          user: post.user,
+          timestamp: post.timestamp,
+          text: post.text,
+          children: []
+        });
+      }
+      else {
+        angular.forEach($scope.posts, function(value, key) {
+          if (value.id == post.parent) {
+            value.children.push({
+              id: snapshot.snapshot.name,
+              user: post.user,
+              timestamp: post.timestamp,
+              text: post.text ? post.text : '',
+              children: []
+            });
+          }
+        });
+      }
+    });
+
+    posts.$on('child_removed', function(snapshot) {
+      var removedPost = snapshot.snapshot.value;
+
+      if (!removedPost.parent) {
+        angular.forEach($scope.posts, function(post, key) {
+          if (post.id == snapshot.snapshot.name) {
+            $scope.posts.splice(key, 1);
+          }
+        });
+      }
+      else {
+        angular.forEach($scope.posts, function(post, key) {
+          if (post.id == removedPost.parent) {
+            angular.forEach(post.children, function(child, key) {
+              if (child.id == snapshot.snapshot.name) {
+                post.children.splice(key, 1);
+              }
+            });
+          }
+        });
+      }
+    });
   });
 
 }]);
